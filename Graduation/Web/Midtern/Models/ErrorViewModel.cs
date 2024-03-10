eb2c20b9f80f772a262cbce4eb21d20716640ead
call me ai|sem <aimur349Gmail.com>
2024-03-10 21:09:27 +0600

    added templates for web

---
+++ Graduation/Web/Midtern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Midtern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
