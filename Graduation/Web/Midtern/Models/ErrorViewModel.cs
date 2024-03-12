a643fce965693908025fa7168a30a8f386329237
call me ai|sem <122848387+IdrissovAimurat@users.noreply.github.com>
2024-03-13 02:14:53 +0600

    Delete Graduation/Web/Midtern directory

--- Graduation/Web/Midtern/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Midtern.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
